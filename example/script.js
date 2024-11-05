cbec368e35d45a601e879dde470bcacfc72b9022
stel723 <ekaterinaoyyyy@yandex.ru>
2024-11-05 21:23:44 +0300

    номера

--- example/script.js
+++ example/script.js
@@ -1,4 +1,5 @@
 let num1 = Number(prompt("Введите первое число:"));
 let num2 = Number(prompt("Введите второе число:"));
-let result = num1 - num2;
-alert("Разность:" + result);
+let num3 = Number(prompt("Введите третье число:"));
+let result = num1-(num2*num3);
+alert("деление:" + result);
