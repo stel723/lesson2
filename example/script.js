4c26f423ba2526ce9c6464385ea16de3668c126c
stel723 <ekaterinaoyyyy@yandex.ru>
2024-11-07 19:29:30 +0300

    ххх

--- example/script.js
+++ example/script.js
@@ -1,5 +1,14 @@
-let num1 = Number(prompt("Введите первое число:"));
-let num2 = Number(prompt("Введите второе число:"));
-let num3 = Number(prompt("Введите третье число:"));
-let result = num1-(num2*num3);
-alert("деление:" + result);
+// let name = "Alice"
+// console.log(name)
+// let num1 = Number(prompt("Введите первое число:"));                                      5 номер
+// let num2 = Number(prompt("Введите второе число:"));
+// let num3 = Number(prompt("Введите третье число:"));
+// let result = num1-(num2*num3);
+// alert("остаток от деления:" + result);
+
+// let num1 = Number(prompt("Введите первое число:"));
+// let num2 = Number(prompt("Введите второе число:"));
+// let num3 = Number(prompt("Введите третье число:"));
+// let result = num1-(num2*num3);
+// alert("остаток от деления:" + result);
+
