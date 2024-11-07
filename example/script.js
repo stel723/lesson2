7a9bc5a538728ec0bf981a84e8b326d7e18800db
stel723 <ekaterinaoyyyy@yandex.ru>
2024-11-07 20:31:00 +0300

    задачи

--- example/script.js
+++ example/script.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 let number1 = Number(prompt('Введите первое число:'));
 let number2 = Number(prompt('Введите второе число:'));
-let number3 = Number(prompt('Введите третье число:'));
-let delenie = number1 - (number2*number3);
-console.log('Остаток от деления:'+ delenie);
+let number3 = Number(prompt('Введите третие число:'));
+let delenie = number1 ** number2;
+console.log('Среднее арифметическое:'+ delenie);
