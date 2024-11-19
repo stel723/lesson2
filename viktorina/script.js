f4e4d406dbdecee21d1ea1b9ac26d7f88b9f152b
stel723 <ekaterinaoyyyy@yandex.ru>
2024-11-19 21:11:52 +0300

    викторина

---
+++ viktorina/script.js
@@ -0,0 +1,23 @@
+const questions = [
+    {
+        question: "Сколько планет в нашей солнечной системе?",
+        options: ["7","8","9"],
+        correctAnswer: 1,
+        image: "images/image1.jpg"
+    },
+    {
+        question: "Какое наименьшее простое число?",
+        options: ["0","1","2"],
+        correctAnswer: 2,
+        image: "images/image2.jpg"
+    },
+    {
+        question: "Что символизирует красный цвет на светофоре?",
+        options: ["Стоп","Поехали","Медленно"],
+        correctAnswer: 0,
+        image: "images/image3.jpg"
+    }
+];
+let currentQuestion = 0;
+let score = 0;
+
